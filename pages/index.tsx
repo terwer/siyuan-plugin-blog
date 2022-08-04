a1f7ff72bf3793894ce6067ef12830282748f7c9
terwer <youweics@163.com>
2022-08-04 18:58:39 +0800

    feat:#14 静态渲染，提升性能

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type {GetServerSideProps, NextPage} from 'next'
+import type {GetServerSideProps, GetStaticProps, NextPage} from 'next'
 import {API} from "../lib/api";
 import {API_TYPE_CONSTANTS} from "../lib/constants";
 import {Post} from "../lib/common/post";
@@ -24,16 +24,48 @@ export default Home
 
 // https://github.com/siyuan-note/siyuan/blob/master/API_zh_CN.md#%E9%89%B4%E6%9D%83
 // https://github.com/vercel/next.js/blob/canary/examples/cms-wordpress/pages/index.js
-export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
-    const query = context.query || {}
-    if (query.t instanceof Array) {
-        throw new Error("参数类型错误")
-    }
+// export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+//     const query = context.query || {}
+//     if (query.t instanceof Array) {
+//         throw new Error("参数类型错误")
+//     }
+//
+//     let cfg: SiteConfig = new SiteConfig()
+//     let result: Array<Post> = []
+//     const type = query.t || API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+//     const pageno = query.p
+//
+//     const api = new API(type)
+//
+//     // 配置
+//     const cfgs = await api.getUsersBlogs() || []
+//     if (cfgs.length > 0) {
+//         cfg.userBlog = cfgs[0]
+//     }
+//     // 文章
+//     if (pageno) {
+//         let num = 1
+//         if (typeof pageno === "string") {
+//             num = parseInt(pageno) || 1
+//         }
+//         result = await api.getRecentPosts(10, num - 1)
+//     } else {
+//         result = await api.getRecentPosts(10)
+//     }
+//
+//     return {
+//         props: {
+//             type: type,
+//             layoutCfg: JSON.parse(JSON.stringify(cfg)),
+//             posts: JSON.parse(JSON.stringify(result))
+//         }
+//     }
+// }
 
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
     let cfg: SiteConfig = new SiteConfig()
     let result: Array<Post> = []
-    const type = query.t || API_TYPE_CONSTANTS.API_TYPE_SIYUAN
-    const pageno = query.p
+    const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
 
     const api = new API(type)
 
@@ -43,15 +75,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
         cfg.userBlog = cfgs[0]
     }
     // 文章
-    if (pageno) {
-        let num = 1
-        if (typeof pageno === "string") {
-            num = parseInt(pageno) || 1
-        }
-        result = await api.getRecentPosts(10, num - 1)
-    } else {
-        result = await api.getRecentPosts(10)
-    }
+    result = await api.getRecentPosts(10)
 
     return {
         props: {
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,8 +1,7 @@
-import {GetServerSideProps, NextPage} from "next";
+import {GetStaticProps, NextPage} from "next";
 import {Post} from "../../lib/common/post";
 import {API} from "../../lib/api";
 import {API_TYPE_CONSTANTS} from "../../lib/constants";
-import log from "../../lib/logUtil";
 import styles from "../../components/themes/default/css/layout.module.css";
 import SiteConfig from "../../lib/common/siteconfig";
 import DefaultLayout from "../../components/themes/default/defaultLayout";
@@ -31,12 +30,46 @@ const PostDetail: NextPage<Props> = (props, context) => {
 
 export default PostDetail
 
-export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
-    const query = context.query || {}
-    if (query.t instanceof Array) {
-        throw new Error("参数类型错误")
-    }
+// export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+//     const query = context.query || {}
+//     if (query.t instanceof Array) {
+//         throw new Error("参数类型错误")
+//     }
+//
+//     const slug = context?.params?.slug
+//     if (!slug || typeof slug !== "string") {
+//         throw new Error("文章路径错误")
+//     }
+//     let smartSlug = ""
+//     if (slug.indexOf(".html") > -1) {
+//         smartSlug = slug.replace(".html", "")
+//     }
+//
+//     const type = query.t || API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+//
+//     const api = new API(type)
+//
+//     let cfg: SiteConfig = new SiteConfig()
+//     // 配置
+//     const cfgs = await api.getUsersBlogs() || []
+//     if (cfgs.length > 0) {
+//         cfg.userBlog = cfgs[0]
+//     }
+//
+//     // 文章
+//     log.logInfo("smartSlug=>", smartSlug)
+//     const post = await api.getPost(smartSlug)
+//
+//     return {
+//         props: {
+//             type: type,
+//             propCfg: JSON.parse(JSON.stringify(cfg)),
+//             post: JSON.parse(JSON.stringify(post))
+//         }
+//     }
+// }
 
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
     const slug = context?.params?.slug
     if (!slug || typeof slug !== "string") {
         throw new Error("文章路径错误")
@@ -46,8 +79,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
         smartSlug = slug.replace(".html", "")
     }
 
-    const type = query.t || API_TYPE_CONSTANTS.API_TYPE_SIYUAN
-
+    const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
     const api = new API(type)
 
     let cfg: SiteConfig = new SiteConfig()
@@ -58,7 +90,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
     }
 
     // 文章
-    log.logInfo("smartSlug=>", smartSlug)
+    // log.logInfo("smartSlug=>", smartSlug)
     const post = await api.getPost(smartSlug)
 
     return {
@@ -67,5 +99,18 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
             propCfg: JSON.parse(JSON.stringify(cfg)),
             post: JSON.parse(JSON.stringify(post))
         }
+    }
+}
+
+export async function getStaticPaths() {
+    const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+    const api = new API(type)
+    const result = await api.getRecentPosts(10)
+
+    // 静态路径
+    const fpath = result.map(({postid}) => `/post/${postid}.html`) || [];
+    return {
+        paths: fpath,
+        fallback: true,
     }
 }
