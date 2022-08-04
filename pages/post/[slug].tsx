69ebedc965e8cf07ecf2e2a4dced71bb26a38f0d
terwer <youweics@163.com>
2022-08-04 21:41:35 +0800

    feat:#14 代码高亮

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,10 +1,14 @@
-import {GetStaticProps, NextPage} from "next";
+import {GetServerSideProps, GetStaticProps, NextPage} from "next";
 import {Post} from "../../lib/common/post";
 import {API} from "../../lib/api";
 import {API_TYPE_CONSTANTS} from "../../lib/constants";
 import styles from "../../components/themes/default/css/layout.module.css";
+import postStyles from "../../components/themes/default/css/post.module.css"
 import SiteConfig from "../../lib/common/siteconfig";
 import DefaultLayout from "../../components/themes/default/defaultLayout";
+import {useEffect} from "react";
+import hljs from 'highlight.js'
+import 'highlight.js/styles/vs.css'
 
 type Props = {
     type: string,
@@ -14,6 +18,20 @@ type Props = {
 
 const PostDetail: NextPage<Props> = (props, context) => {
 
+    useEffect(() => {
+        // 配置 highlight.js
+        hljs.configure({
+            // 忽略未经转义的 HTML 字符
+            ignoreUnescapedHTML: true
+        })
+        // 获取到内容中所有的code标签
+        const codes = document.querySelectorAll('pre code')
+        codes.forEach((el) => {
+            // 让code进行高亮
+            hljs.highlightElement(el as HTMLElement)
+        })
+    }, [])
+
     function createMarkup() {
         return {__html: props.post?.description};
     }
@@ -22,7 +40,7 @@ const PostDetail: NextPage<Props> = (props, context) => {
         <DefaultLayout props={props.propCfg}>
             <main className={styles.main}>
                 <p>{props.post?.mt_keywords}</p>
-                <div dangerouslySetInnerHTML={createMarkup()}/>
+                <div className={postStyles.postBody} dangerouslySetInnerHTML={createMarkup()}/>
             </main>
         </DefaultLayout>
     )
@@ -30,46 +48,12 @@ const PostDetail: NextPage<Props> = (props, context) => {
 
 export default PostDetail
 
-// export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
-//     const query = context.query || {}
-//     if (query.t instanceof Array) {
-//         throw new Error("参数类型错误")
-//     }
-//
-//     const slug = context?.params?.slug
-//     if (!slug || typeof slug !== "string") {
-//         throw new Error("文章路径错误")
-//     }
-//     let smartSlug = ""
-//     if (slug.indexOf(".html") > -1) {
-//         smartSlug = slug.replace(".html", "")
-//     }
-//
-//     const type = query.t || API_TYPE_CONSTANTS.API_TYPE_SIYUAN
-//
-//     const api = new API(type)
-//
-//     let cfg: SiteConfig = new SiteConfig()
-//     // 配置
-//     const cfgs = await api.getUsersBlogs() || []
-//     if (cfgs.length > 0) {
-//         cfg.userBlog = cfgs[0]
-//     }
-//
-//     // 文章
-//     log.logInfo("smartSlug=>", smartSlug)
-//     const post = await api.getPost(smartSlug)
-//
-//     return {
-//         props: {
-//             type: type,
-//             propCfg: JSON.parse(JSON.stringify(cfg)),
-//             post: JSON.parse(JSON.stringify(post))
-//         }
-//     }
-// }
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+    const query = context.query || {}
+    if (query.t instanceof Array) {
+        throw new Error("参数类型错误")
+    }
 
-export const getStaticProps: GetStaticProps<Props> = async (context) => {
     const slug = context?.params?.slug
     if (!slug || typeof slug !== "string") {
         throw new Error("文章路径错误")
@@ -79,7 +63,8 @@ export const getStaticProps: GetStaticProps<Props> = async (context) => {
         smartSlug = slug.replace(".html", "")
     }
 
-    const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+    const type = query.t || API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+
     const api = new API(type)
 
     let cfg: SiteConfig = new SiteConfig()
@@ -102,15 +87,48 @@ export const getStaticProps: GetStaticProps<Props> = async (context) => {
     }
 }
 
-export async function getStaticPaths() {
-    const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
-    const api = new API(type)
-    const result = await api.getRecentPosts(10)
-
-    // 静态路径
-    const fpath = result.map(({postid}) => `/post/${postid}.html`) || [];
-    return {
-        paths: fpath,
-        fallback: true,
-    }
-}
+// export const getStaticProps: GetStaticProps<Props> = async (context) => {
+//     const slug = context?.params?.slug
+//     if (!slug || typeof slug !== "string") {
+//         throw new Error("文章路径错误")
+//     }
+//     let smartSlug = ""
+//     if (slug.indexOf(".html") > -1) {
+//         smartSlug = slug.replace(".html", "")
+//     }
+//
+//     const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+//     const api = new API(type)
+//
+//     let cfg: SiteConfig = new SiteConfig()
+//     // 配置
+//     const cfgs = await api.getUsersBlogs() || []
+//     if (cfgs.length > 0) {
+//         cfg.userBlog = cfgs[0]
+//     }
+//
+//     // 文章
+//     // log.logInfo("smartSlug=>", smartSlug)
+//     const post = await api.getPost(smartSlug)
+//
+//     return {
+//         props: {
+//             type: type,
+//             propCfg: JSON.parse(JSON.stringify(cfg)),
+//             post: JSON.parse(JSON.stringify(post))
+//         }
+//     }
+// }
+//
+// export async function getStaticPaths() {
+//     const type = API_TYPE_CONSTANTS.API_TYPE_SIYUAN
+//     const api = new API(type)
+//     const result = await api.getRecentPosts(10)
+//
+//     // 静态路径
+//     const fpath = result.map(({postid}) => `/post/${postid}.html`) || [];
+//     return {
+//         paths: fpath,
+//         fallback: true,
+//     }
+// }
