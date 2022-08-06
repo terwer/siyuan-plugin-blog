be4920a316f7675c7a073fbc82933bb080276d76
terwer <youweics@163.com>
2022-08-06 23:22:14 +0800

    feat:#27 添加fork me on Github

--- components/themes/default/defaultHeader.tsx
+++ components/themes/default/defaultHeader.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import DefaultNavbar from "./defaultNavbar";
 import SiteConfig from "../../../lib/common/siteconfig";
+import Image from "next/image";
+import headerStyles from "./css/header.module.css"
 
 export default function DefaultHeader({props,keyword}: { props: SiteConfig,keyword?:string }) {
     return (
@@ -11,6 +13,14 @@ export default function DefaultHeader({props,keyword}: { props: SiteConfig,keywo
                 <meta name="description" content={props?.description}/>
             </Head>
             <header>
+                <div className={headerStyles.forkMe}>
+                    <a href="https://github.com/you">
+                        <Image loading="lazy" width="149" height="149"
+                               src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149"
+                               className="attachment-full size-full" alt="Fork me on GitHub"
+                               data-recalc-dims="1"/>
+                    </a>
+                </div>
                 <DefaultNavbar props={props}/>
             </header>
         </>
