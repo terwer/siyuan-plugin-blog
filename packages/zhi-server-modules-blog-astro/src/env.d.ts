f9c4f8e035baeaa03b0a49d5fcc81154c1ae3368
terwer <youweics@163.com>
2023-04-09 13:28:52 +0800

    feat: Add stylint

--- packages/zhi-server-modules-blog-astro/src/env.d.ts
+++ packages/zhi-server-modules-blog-astro/src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_LOG_LEVEL: string
+  readonly VITE_DEBUG_MODE: string
+
+  readonly VITE_SIYUAN_API_URL: string
+  readonly VITE_SIYUAN_AUTH_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
