c28606edb136225c16276ed2a5e9d9f00ece05a8
terwer <youweics@163.com>
2023-04-16 00:13:08 +0800

    chore(zhi-env): release zhi-env 1.13.2

--- apps/zhi-env/esbuild.config.cjs
+++ apps/zhi-env/esbuild.config.cjs
@@ -24,49 +24,14 @@
  */
 
 const path = require("path")
-// const minimist = require("minimist")
 const { dtsPlugin } = require("esbuild-plugin-d.ts")
-const { copy } = require("esbuild-plugin-copy")
-
-// const args = minimist(process.argv.slice(2))
-// const isWatch = args.watch || args.w
-
-// for dist
-const baseDir = "./"
-const distDir = path.join(baseDir, "dist")
-
-// for outer custom output for dev
-// const baseDir = isWatch ? "my-custom-absolute-path" : "./"
-// const distDir = isWatch ? baseDir : path.join(baseDir, "dist")
 
 /**
  * 构建配置
  */
 module.exports = {
   entryPoints: ["src/index.ts"],
-  outfile: path.join(distDir, "index.js"),
-  bundle: true,
+  outfile: "dist/index.js",
   format: "esm",
-  platform: "node",
-  plugins: [
-    dtsPlugin(),
-    copy({
-      // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
-      // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
-      resolveFrom: "cwd",
-      assets: [
-        // copy folder
-        {
-          from: "./public/**/*",
-          to: [path.join(distDir, "assets")],
-        },
-        // copy one file
-        {
-          from: ["./README.md"],
-          to: [path.join(distDir, "/README.md")],
-        },
-      ],
-      watch: true,
-    }),
-  ],
+  plugins: [dtsPlugin()],
 }
