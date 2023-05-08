a3e1dd45468aa59c8fbfd10ac26752f2bdb87614
terwer <youweics@163.com>
2023-05-08 14:59:48 +0800

    feat: add vercel build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
+const isDev = process.env.NODE_ENV === "development"
+const isVercelBuild = process.env.BUILD_TYPE === "vercel"
+const isSiyuanBuild = process.env.BUILD_TYPE === "siyuan"
+const isSsr = isDev || isVercelBuild
+
+const ssrPreset = isVercelBuild ? "vercel" : isDev ? "node-server" : undefined
+const ssrServeStatic = isSiyuanBuild
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    ssr: isSsr,
+    // https://nuxt.com/docs/guide/going-further/custom-routing#hash-mode-spa
+    router: {
+        options: {
+            hashMode: !isSsr,
+        },
+    },
+    nitro: {
+        preset: ssrPreset,
+        // 开启之后将进行静态伺服
+        serveStatic: ssrServeStatic,
+    },
 })
