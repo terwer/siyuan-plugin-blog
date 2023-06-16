373af11fe5828c7c425ef3ff8df97bc1bda764f8
terwer <youweics@163.com>
2023-06-16 22:31:28 +0800

    feat: 在线分享第一版-完善打包流程

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,11 @@ echo "Nuxt build finished."
 pnpm pluginBuild
 echo "Plugin build finished."
 
+# 拷贝资源
+# rsync -av --progress .output/public/ ./dist/
+rsync -av .output/public/ ./dist/
+echo "Resources are copied."
+
 # 使用 `sed` 命令替换内容
 find ./dist/_nuxt -type f -name 'entry.*.js' -exec \
   sed -i '' -E 's/\/__i18n__\/prerender/\/plugins\/siyuan-blog\/__i18n__\/prerender/g' {} \;
