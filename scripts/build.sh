ec7d4668a6f5b0fcc3d5b3509c605915c27c89b1
terwer <youweics@163.com>
2023-06-16 22:26:46 +0800

    feat: 在线分享第一版-完善打包流程

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,18 +10,4 @@ find ./dist/_nuxt -type f -name 'entry.*.js' -exec \
   sed -i '' -E 's/\/__i18n__\/prerender/\/plugins\/siyuan-blog\/__i18n__\/prerender/g' {} \;
 echo "The i18n path has been replaced"
 
-# 添加 html 版本号
-function replace_file_name() {
-    timestamp="$(date +%s)"
-    sed -i '' -E \
-        -e "s/(<script[^>]*src=[\"'][^\"']+\.js)[^\"']*(\"[^>]*>)/\1?v=${timestamp}\2/g" \
-        -e "s/(<link[^>]*href=[\"'][^\"']+\.js)[^\"']*(\"[^>]*>)/\1?v=${timestamp}\2/g" \
-        -e "s/(<link[^>]*href=[\"'][^\"']+\.css)[^\"']*(\"[^>]*>)/\1?v=${timestamp}\2/g" \
-        -e "s/(<link[^>]*href=[\"'][^\"']+\.(svg|png|jpe?g|gif|webp)[^\"']*[\"'][^>]*>)/\1?v=${timestamp}\"/g" \
-        "./dist/index.html"
-}
-
-replace_file_name
-echo "The html version number has been added"
-
 echo "Siyuan build success."
