bcc2c5a7a527dabee1406a8351164a9d6b0831d5
terwer <youweics@163.com>
2023-06-16 22:39:00 +0800

    feat: 在线分享第一版-完善打包流程

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,8 @@ rsync -av .output/public/ ./dist/
 echo "Resources are copied."
 
 # 使用 `sed` 命令替换内容
-ll ./dist/_nuxt
+sleep 2s
+ls ./dist/_nuxt
 find ./dist/_nuxt -type f -name 'entry.*.js' -exec \
   sed -i '' -E 's/\/__i18n__\/prerender/\/plugins\/siyuan-blog\/__i18n__\/prerender/g' {} \;
 echo "The i18n path has been replaced"
