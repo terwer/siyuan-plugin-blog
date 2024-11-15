d6bdda907bd7f8ad8a6cd30786ad9559a9881846
terwer <youweics@163.com>
2024-11-15 18:25:07 +0800

    fix: outline not show normal in some case

--- components/static/Outline.vue
+++ components/static/Outline.vue
@@ -4,7 +4,7 @@
       <a class="outline-title-link"> {{ "文档大纲" }} </a>
     </div>
     <div v-for="(item, index) in outlineData" :key="index">
-      <outline-item :item="item" :max-depth="maxDepth" :is-root="true" />
+      <outline-item :item="item" :max-depth="maxDepth" :root-level="getRootLevel(item)" :is-root="true" />
     </div>
   </div>
 </template>
@@ -23,12 +23,33 @@ const props = defineProps({
   },
 })
 
+const getRootLevel = () => {
+  if (props.outlineData.length === 0) return 1 // 如果数据为空，返回默认级别 1
+
+  const levels = props.outlineData.map((item) => getItemLevel(item))
+  const uniqueLevels = new Set(levels)
+
+  if (uniqueLevels.size === 1) {
+    // 所有项的级别相同，返回该级别
+    return levels[0]
+  } else {
+    // 返回最小的级别
+    return Math.min(...levels)
+  }
+}
+const getItemLevel = (item) => {
+  const level = parseInt(item.subType.replace("h", ""), 10)
+  return isNaN(level) ? 1 : level // 默认级别为1
+}
+
 onMounted(() => {})
 </script>
 
 <style lang="stylus" scoped>
 .outline-title
+  margin-bottom 0.5rem
   .outline-title-link
     text-decoration none
     color #333
+    font-weight 600
 </style>
