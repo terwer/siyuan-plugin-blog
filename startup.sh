19a55ac41600ada43ca850d1671ef76455151d0c
terwer <youweics@163.com>
2024-11-16 16:40:54 +0800

    chore: no docTree hidden

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 # nohup node .output/server/index.mjs > /dev/null 2>&1 &
 
-cd /home/admin/blog/share-front
-./node_modules/pm2/bin/pm2 list
-./node_modules/pm2/bin/pm2 start .output/server/index.mjs
+pm2 list
+pm2 start .output/server/index.mjs
