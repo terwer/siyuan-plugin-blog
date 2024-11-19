ba733da49c835bff45068966c1d13d01a80de654
terwer <youweics@163.com>
2024-11-19 10:49:15 +0800

    fix: pm2 script

--- startup.sh
+++ startup.sh
@@ -1,23 +1,28 @@
-# nohup node .output/server/index.mjs > /dev/null 2>&1 &
+#!/bin/bash
 
-# Define the application name
-APP_NAME="share-front"
+# 定义应用名称
+APP_NAME="share-front2"
 
-# List currently running applications
+# 列出当前运行的应用
+echo "Listing currently running applications..."
 pm2 list
 
-# Check if the application exists
+# 检查应用是否存在
 if pm2 describe "$APP_NAME" &> /dev/null; then
-  # Stop the application
+  # 停止应用
+  echo "Stopping application '$APP_NAME'..."
   pm2 stop "$APP_NAME"
 
-  # Delete the application
+  # 删除应用
+  echo "Deleting application '$APP_NAME'..."
   pm2 delete "$APP_NAME"
   echo "Application '$APP_NAME' has been stopped and deleted"
 else
   echo "Application '$APP_NAME' does not exist, skipping stop and delete steps"
 fi
 
-# Start the new application
-pm2 start .output/server/index.mjs --name "$APP_NAME"
+# 启动新应用
+echo "Starting new application '$APP_NAME'..."
+pm2 start pm2.json --update-env
+
 echo "Application '$APP_NAME' has been started"
