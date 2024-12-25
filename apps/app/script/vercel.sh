050cdee78cf52e4ffbf797cbb348599c09ce28ba
terwer <youweics@163.com>
2024-12-25 17:54:28 +0800

    feat: app support vercel and cloudflare

--- apps/app/script/vercel.sh
+++ apps/app/script/vercel.sh
@@ -5,12 +5,3 @@ echo "Using Vercel build config as SSR build."
 cp nuxt.vercel.config.ts nuxt.config.ts
 nuxt build
 echo "Nuxt build for vercel finished."
-
-# 拷贝资源
-# rsync -av --progress .output/public/ ./dist/
-mkdir -p ../../dist/vercel
-#rsync -av .vercel/output/ ../../dist/vercel
-cp -r .vercel/output/ ../../dist/vercel
-echo "Resources are copied."
-
-echo "Vercel build finished."
