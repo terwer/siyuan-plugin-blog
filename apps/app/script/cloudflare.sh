029959f474a122d75fd0ec90c286f0f0a3ea1ef8
terwer <youweics@163.com>
2024-12-25 17:50:28 +0800

    feat: app support vercel and cloudflare

--- apps/app/script/cloudflare.sh
+++ apps/app/script/cloudflare.sh
@@ -9,7 +9,7 @@ echo "Nuxt build for cloudflare finished."
 # 拷贝资源
 # rsync -av --progress .output/public/ ./dist/
 mkdir -p ../../dist/cloudflare
-#rsync -av dist/ ../../dist/cloudflare
+# rsync -av dist/ ../../dist/cloudflare
 cp -r dist/ ../../dist/cloudflare
 echo "Resources are copied."
 
