e766677515e068c13ba09279b241e646585a0e37
terwer <youweics@163.com>
2024-12-25 18:31:29 +0800

    feat: app support vercel and cloudflare

--- apps/app/script/cloudflare.sh
+++ apps/app/script/cloudflare.sh
@@ -5,12 +5,3 @@ echo "Using Cloudflare build config as SSR build."
 cp nuxt.cloudflare.config.ts nuxt.config.ts
 nuxt build
 echo "Nuxt build for cloudflare finished."
-
-# 拷贝资源
-# rsync -av --progress .output/public/ ./dist/
-mkdir -p ../../dist/cloudflare
-# rsync -av dist/ ../../dist/cloudflare
-cp -r dist/ ../../dist/cloudflare
-echo "Resources are copied."
-
-echo "Cloudflare build finished."
