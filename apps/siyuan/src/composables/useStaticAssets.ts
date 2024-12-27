519e53164052084ca5b75710097c789786990666
terwer <youweics@163.com>
2024-12-27 14:44:53 +0800

    feat: add right menu

--- apps/siyuan/src/composables/useStaticAssets.ts
+++ apps/siyuan/src/composables/useStaticAssets.ts
@@ -1,3 +1,12 @@
+/*
+ *            GNU GENERAL PUBLIC LICENSE
+ *               Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2024 Terwer, Inc. <https://terwer.space/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ */
+
 import * as cheerio from "cheerio"
 import {createAppLogger} from "../utils/appLogger.ts"
 import {useSiyuanApi} from "./useSiyuanApi.ts"
--- apps/siyuan/src/stores/useSettingStore.ts
+++ apps/siyuan/src/stores/useSettingStore.ts
@@ -1,3 +1,12 @@
+/*
+ *            GNU GENERAL PUBLIC LICENSE
+ *               Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2024 Terwer, Inc. <https://terwer.space/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ */
+
 import {computed, ref} from "vue"
 import {useSiyuanApi} from "../composables/useSiyuanApi.ts"
 import {createAppLogger} from "../utils/appLogger.ts"
