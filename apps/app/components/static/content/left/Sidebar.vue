ae2ca82927e095a567f53bec6ce6f1207a3aa6f6
terwer <youweics@163.com>
2025-01-06 11:59:39 +0800

    feat: new document tree

--- apps/app/components/static/content/left/Sidebar.vue
+++ apps/app/components/static/content/left/Sidebar.vue
@@ -16,41 +16,39 @@ const props = defineProps<{ post: any, setting: typeof AppConfig }>()
 
 const { t } = useI18n()
 
-interface MenuData {
-  index: string;
-  title: string;
-  children?: MenuData[];
-}
-
-const menuData = ref<MenuData[]>([
-  {
-    index: "1",
-    title: "Navigator One.也是坑很成才分手的方式地方都是粉丝的方式地方",
-    children: [
-      { index: "1-1", title: "Option 1" },
-      {
-        index: "1-2",
-        title: "Option 2。选项很长也有可能是啥放松放松的蛋糕的哥哥哥峰哥峰哥发豆腐干豆腐",
-      },
-    ],
-  },
-  {
-    index: "2",
-    title: "Navigator Two",
-    children: [
-      { index: "2-1", title: "Option 1" },
-      { index: "2-2", title: "Option 2" },
-    ],
-  },
-  {
-    index: "3",
-    title: "Navigator Three",
-    children: [
-      { index: "3-1", title: "Option 1" },
-      { index: "3-2", title: "Option 2" },
-    ],
-  },
-])
+const formData = reactive({
+  openedIds: [], // 默认展开的节点
+  activeIndex: "1-1", // 默认选中
+  menuData: [
+    {
+      index: "1",
+      title: "Navigator One.也是坑很成才分手的方式地方都是粉丝的方式地方",
+      children: [
+        { index: "1-1", title: "Option 1" },
+        {
+          index: "1-2",
+          title: "Option 2。选项很长也有可能是啥放松放松的蛋糕的哥哥哥峰哥峰哥发豆腐干豆腐",
+        },
+      ],
+    },
+    {
+      index: "2",
+      title: "Navigator Two",
+      children: [
+        { index: "2-1", title: "Option 1" },
+        { index: "2-2", title: "Option 2" },
+      ],
+    },
+    {
+      index: "3",
+      title: "Navigator Three",
+      children: [
+        { index: "3-1", title: "Option 1" },
+        { index: "3-2", title: "Option 2" },
+      ],
+    },
+  ],
+})
 </script>
 
 <template>
@@ -60,8 +58,8 @@ const menuData = ref<MenuData[]>([
       {{ t("static.outline") }}
     </div>
     <!-- 菜单部分 -->
-    <el-menu class="sidebar-menu" :default-openeds="['1', '3']">
-      <SidebarMenu v-for="menu in menuData" :key="menu.index" :menu="menu" />
+    <el-menu class="sidebar-menu" :default-openeds="formData.openedIds" :default-active="formData.activeIndex">
+      <SidebarMenu v-for="menu in formData.menuData" :key="menu.index" :menu="menu" />
     </el-menu>
   </el-scrollbar>
 </template>
